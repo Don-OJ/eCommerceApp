f3db5c885ca1dc09ae0592f4d9795c245c06d296
Picto Judiciary <picto_jotg@outlook.com>
2025-03-01 07:51:47 +0000

    Added the application user AKA AppUser in the domain.

---
+++ eCommerceApp.Domain/Entities/Identity/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace eCommerceApp.Domain.Entities.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        public string Fullname { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public bool IsBoss { get; set; } = false;
+    }
+}
